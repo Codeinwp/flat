9c71736bd4ea6d592cd9af6b81d8ee520ba4b125
Ashfame <mail@ashfame.com>
2014-02-26 06:03:32 +0530

    added a filter on showing credits in footer

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
-				<div class="site-info">
-					<?php do_action( 'flat_credits' ); ?>
-					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flat' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flat' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flat' ), 'WordPress' ); ?></a>.
-					<?php printf( __( 'Theme: %1$s by %2$s.', 'flat' ), 'Flat', '<a href="'.esc_url( 'http://www.yoarts.com/' ).'" title="'.esc_attr('Webmaster Tutorials & Resources').'">YoArts</a>' ); ?>
-				</div><!-- .site-info -->
+				<?php if ( apply_filters( 'show_flat_credits', true ) ) { ?>
+					<div class="site-info">
+						<?php do_action( 'flat_credits' ); ?>
+						<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'flat' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'flat' ); ?>"><?php printf( __( 'Proudly powered by %s', 'flat' ), 'WordPress' ); ?></a>.
+						<?php printf( __( 'Theme: %1$s by %2$s.', 'flat' ), 'Flat', '<a href="'.esc_url( 'http://www.yoarts.com/' ).'" title="'.esc_attr('Webmaster Tutorials & Resources').'">YoArts</a>' ); ?>
+					</div><!-- .site-info -->
+				<?php } ?>
 			</div>
 		</div>
 	</div>
