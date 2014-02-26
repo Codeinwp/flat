2b8e3a350fd9cabde19a9998b0838927a36100b4
Ashfame <mail@ashfame.com>
2014-02-26 05:42:10 +0530

    Not do anything with title, SEO is best handled by WordPress SEO by Yoast
    Why? Read this - https://yoast.com/wordpress-seo-plugin-theme-integration-guide/#head

--- functions.php
+++ functions.php
@@ -88,25 +88,6 @@ function flat_paging_nav() {
 	<?php
 }
 
-function flat_wp_title( $title, $sep ) {
-	global $paged, $page;
-
-	if ( is_feed() )
-		return $title;
-
-	$title .= get_bloginfo( 'name' );
-
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title = "$title $sep $site_description";
-
-	if ( $paged >= 2 || $page >= 2 )
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'flat' ), max( $paged, $page ) );
-
-	return $title;
-}
-add_filter( 'wp_title', 'flat_wp_title', 10, 2 );
-
 if ( ! function_exists( 'flat_post_nav' ) ) :
 function flat_post_nav() {
     global $post;
