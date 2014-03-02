66c0d9bad6f3ea7215e28fc00bee9a7f78232a7d
yoarts <thanh@yoarts.com>
2014-03-02 20:39:48 +0700

    Wrap up the javascript code as comment of @ashfame

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,17 @@
-jQuery(document).ready(function($) {
-  $('.toggle-sidebar').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
+(function($){
+  $(document).ready(function() {
+    $('.toggle-sidebar').click(function() {
+      $('.row-offcanvas').toggleClass('active');
+    });
 
-  $('.toggle-navigation').click(function() {
-    $(this).toggleClass('open').next('#site-navigation').slideToggle(300);
-  });
+    $('.toggle-navigation').click(function() {
+      $(this).toggleClass('open').next('#site-navigation').slideToggle(300);
+    });
 
-  $('#site-navigation .sub-menu').before('<i class="fa fa-caret-right"></i>');
+    $('#site-navigation .sub-menu').before('<i class="fa fa-caret-right"></i>');
 
-  $('#site-navigation .fa').click(function() {
-    $(this).toggleClass('open').next('ul').slideToggle(300);
+    $('#site-navigation .fa').click(function() {
+      $(this).toggleClass('open').next('ul').slideToggle(300);
+    });
   });
-});
+})(jQuery);
