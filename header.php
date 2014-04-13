bb847e846012e35793d3cc54b01302db8bc16388
Jackie Lord <j4ckielord@gmail.com>
2014-04-13 11:42:31 +0700

    Add theme support IE8

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title><?php wp_title(''); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
