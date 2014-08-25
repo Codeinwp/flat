65d03c5f15a78709576f2ee324e242b412d6c050
Tobias Kerst <tobias.kerst@tobscore.com>
2014-08-25 18:09:18 +0200

    Includes spaces so Travis CI will pass

--- inc/customize.php
+++ inc/customize.php
@@ -50,9 +50,9 @@ function flat_customize_register( $wp_customize ) {
 		'section' => 'title_tagline',
 		'type'    => 'select',
 		'choices'    => array(
-			'site_title' => __('Site Title', 'flat'),
-			'site_logo' => __('Site Logo', 'flat'),
-			'both_title_logo' => __('Both Title & Logo', 'flat'),
+			'site_title' => __( 'Site Title', 'flat' ),
+			'site_logo' => __( 'Site Logo', 'flat' ),
+			'both_title_logo' => __( 'Both Title & Logo', 'flat' ),
 		),
 	));
 	$wp_customize->add_setting('flat_theme_options[favicon]', array(
@@ -86,9 +86,9 @@ function flat_customize_register( $wp_customize ) {
 		'section' => 'background_image',
 		'type'    => 'radio',
 		'choices'    => array(
-			'cover' => __('Cover', 'flat'),
-			'contain' => __('Contain', 'flat'),
-			'initial' => __('Initial', 'flat'),
+			'cover' => __( 'Cover', 'flat' ),
+			'contain' => __( 'Contain', 'flat' ),
+			'initial' => __( 'Initial', 'flat' ),
 		),
 	));
 	$wp_customize->add_section('typography', array(
