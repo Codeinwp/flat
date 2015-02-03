448bbb47cb7b64d0a7a37103b09bd6ed15a7f317
Rick Beckman <rick.beckman@gmail.com>
2015-02-02 23:45:08 -0500

    Functions.php Tweaks
    * Comment functions
    * Make indention consistent
    * Adjust order of comparison operator to fit WP coding standard
    * Pluralize “0 Comment” — also updated in the PO file. Other languages
    shouldn’t be affected — presumably translators would have used the
    correct form when translating the string.

--- dist/functions.php
+++ dist/functions.php
@@ -1,9 +1,17 @@
 <?php
 require get_template_directory() . '/inc/customize.php';
 
-if ( ! isset( $content_width ) ) { $content_width = 720; }
+/**
+ * Set the content width
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 720;
+}
 
 if ( ! function_exists( 'flat_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 */
 	function flat_setup() {
 		load_theme_textdomain( 'flat', get_template_directory() . '/languages' );
 
@@ -31,6 +39,9 @@ endif;
 add_action( 'after_setup_theme', 'flat_setup' );
 
 if ( ! function_exists( 'flat_widgets_init' ) ) :
+	/**
+	 * Registers our sidebar with WordPress
+	 */
 	function flat_widgets_init() {
 		register_sidebar( array(
 			'name'          => __( 'Main Widget Area', 'flat' ),
@@ -46,6 +57,9 @@ endif;
 add_action( 'widgets_init', 'flat_widgets_init' );
 
 if ( ! function_exists( 'flat_scripts_styles' ) ) :
+	/**
+	 * Sets up necessary scripts and styles
+	 */
 	function flat_scripts_styles() {
 		$version = wp_get_theme()->get( 'Version' );
 
@@ -53,7 +67,7 @@ if ( ! function_exists( 'flat_scripts_styles' ) ) :
 			wp_enqueue_script( 'comment-reply' );
 		}
 
-		if ( defined( 'WP_ENV' ) && 'development' === WP_ENV ) {
+		if ( defined( 'WP_ENV' ) && WP_ENV === 'development' ) {
 			$assets = array(
 				'css' => '/assets/css/flat.css',
 				'js'  => '/assets/js/flat.js',
@@ -64,6 +78,7 @@ if ( ! function_exists( 'flat_scripts_styles' ) ) :
 				'js'  => '/assets/js/flat.min.js',
 			);
 		}
+
 		wp_enqueue_style( 'flat-fonts', flat_fonts_url(), array(), null );
 		wp_enqueue_style( 'flat-theme', get_template_directory_uri() . $assets['css'], array(), $version );
 		wp_enqueue_style( 'flat-style', get_stylesheet_uri(), array(), $version );
@@ -73,20 +88,26 @@ endif;
 add_action( 'wp_enqueue_scripts', 'flat_scripts_styles' );
 
 if ( ! function_exists( 'flat_entry_meta' ) ) :
-function flat_entry_meta( $show_sep = true ) {
-	printf( __( '<span class="entry-date"><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s">%3$s</time></a></span> by <span class="byline"><span class="author vcard"><a class="url fn n" href="%4$s" rel="author">%5$s</a></span></span>', 'flat' ),
-		esc_url( get_permalink() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		get_the_author()
-	);
+	/**
+	 * Template tag to output entry metadata
+	 *
+	 * @param bool $show_sep Whether to show a separator between meta items
+	 */
+	function flat_entry_meta( $show_sep = true ) {
+		printf( __( '<span class="entry-date"><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s">%3$s</time></a></span> by <span class="byline"><span class="author vcard"><a class="url fn n" href="%4$s" rel="author">%5$s</a></span></span>', 'flat' ),
+			esc_url( get_permalink() ),
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() ),
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			get_the_author()
+		);
 
-	if ( true === $show_sep ) {
-		echo '<span class="sep">&middot;</span>';
-	}
+		if ( true === $show_sep ) {
+			echo '<span class="sep">&middot;</span>';
+		}
+
 		echo '<span class="comments-link">';
-		comments_popup_link( __( '0 Comment', 'flat' ), __( '1 Comment', 'flat' ), __( '% Comments', 'flat' ) );
+		comments_popup_link( __( '0 Comments', 'flat' ), __( '1 Comment', 'flat' ), __( '% Comments', 'flat' ) );
 		echo '</span>';
-}
+	}
 endif;
