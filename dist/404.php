336b458a6ab793bdda88f7b1349534c934391765
Rick Beckman <rick.beckman@gmail.com>
2015-02-02 23:19:37 -0500

    Lesser Title on 404 Pages
    Demote the page title on 404 pages to an H2 — it shouldn’t be equal to
    the site heading.
    
    (Also removes newline at end of file.)

--- dist/404.php
+++ dist/404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<div id="content" class="site-content" role="main">
-		<h1 class="page-title"><?php _e( 'Not Found', 'flat' ); ?></h1>
+		<h2 class="page-title"><?php _e( 'Not Found', 'flat' ); ?></h2>
 		<div class="page-content">
 			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'flat' ); ?></p>
 			<?php get_search_form(); ?>
