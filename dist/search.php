a78a9e259b2a7df5a761ced66c33d4b545a61098
thanhluu <thanhlt90@gmail.com>
2015-02-09 21:14:06 +0700

    Fix #69: Search Results Pagination

--- dist/search.php
+++ dist/search.php
@@ -1,14 +1,15 @@
 <?php get_header(); ?>
 	<?php flat_hook_search_before(); ?>
+	<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'flat' ), get_search_query() ); ?></h1>
 	<div id="content" class="site-content" role="main">
 		<?php flat_hook_search_top(); ?>
 
 	<?php if ( have_posts() ) : ?>
-		<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'flat' ), get_search_query() ); ?></h1>
+
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'content', get_post_format() ); ?>
 		<?php endwhile; ?>
-		<?php flat_paging_nav(); ?>
+		<?php the_posts_pagination( array( 'prev_text' => __( '<i class="fa fa-chevron-left"></i>', 'flat' ), 'next_text' => __( '<i class="fa fa-chevron-right"></i>', 'flat' ) ) ); ?>
 	<?php else : ?>
 		<?php get_template_part( 'content', 'none' ); ?>
 	<?php endif; ?>
