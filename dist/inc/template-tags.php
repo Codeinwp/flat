09f7168b05b8f9e80001b6747fd4dc631e256b91
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-11 00:41:15 -0200

    fixing last error of Travis

--- dist/inc/template-tags.php
+++ dist/inc/template-tags.php
@@ -170,7 +170,7 @@ if ( ! function_exists( 'the_post_navigation' ) ) :
 	 *                    arguments. Default empty array.
 	 */
 	function the_post_navigation( $args = array() ) {
-		echo get_the_post_navigation( $args );
+		echo balanceTags( get_the_post_navigation( $args ) );
 	}
 endif;
 
@@ -229,7 +229,7 @@ if ( ! function_exists( 'the_posts_pagination' ) ) :
 	 *                    Default empty array.
 	 */
 	function the_posts_pagination( $args = array() ) {
-		echo get_the_posts_pagination( $args );
+		echo balanceTags( get_the_posts_pagination( $args ) );
 	}
 endif;
 
@@ -238,7 +238,7 @@ if ( ! function_exists( 'flat_output_404_content' ) ) :
 	 * The default content of a 404 page
 	 */
 	function flat_output_404_content() {
-		echo '<p>' . __( 'It looks like nothing was found at this location. Maybe try a search?', 'flat' ) . "</p>\n";
+		echo balanceTags( '<p>' . __( 'It looks like nothing was found at this location. Maybe try a search?', 'flat' ) . "</p>\n" );
 
 		get_search_form();
 	}
