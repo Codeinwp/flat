1808d1016c71c35f7fb1edea67fd2a68054aeaae
Rick Beckman <rick.beckman@gmail.com>
2015-02-17 23:49:19 -0500

    * Replace HTML entity arrow in author bio box w/ a Font Awesome icon * Fix various sanitization issues * Ensure the correct version is displayed in the site footer, even if a child theme is displayed * Add i18n to a string in `footer.php`

--- dist/content-none.php
+++ dist/content-none.php
@@ -1,19 +1,19 @@
-<h1 class="page-title"><?php _e( 'Nothing Found', 'flat' ); ?></h1>
+<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'flat' ); ?></h1>
 
 <div class="page-content">
 	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'flat' ), admin_url( 'post-new.php' ) ); ?></p>
+	<p><?php printf( esc_html__( 'Ready to publish your first post? %s', 'flat' ), printf( '<a href="%s">' . esc_html__( 'Get started here', 'flat' ) . '</a>', admin_url( 'post-new.php' ) ) ); ?></p>
 
 	<?php elseif ( is_search() ) : ?>
 
-	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'flat' ); ?></p>
+	<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'flat' ); ?></p>
 	<?php get_search_form(); ?>
 
 	<?php else : ?>
 
-	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'flat' ); ?></p>
+	<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'flat' ); ?></p>
 	<?php get_search_form(); ?>
 
 	<?php endif; ?>
-</div><!-- .page-content -->
+</div>
