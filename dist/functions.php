3502174490c9ce675a6ebef8e8c9c6b195ad6cb2
Rick Beckman <rick.beckman@gmail.com>
2015-02-21 11:54:17 -0500

    Remove output from `die()`
    Not only is the text not able to be translated, but it also won’t have
    access to WordPress’ sanitization functions. If someone is accessing a
    theme file directly, they don’t need an explanation for why they are
    being shown a blank page.

--- dist/functions.php
+++ dist/functions.php
@@ -10,7 +10,7 @@
 # Prevent direct access to this file
 if ( 1 == count( get_included_files() ) ) {
 	header( 'HTTP/1.1 403 Forbidden' );
-	die( 'Direct access of this file is prohibited. Thank you.' );
+	die();
 }
 
 /**
